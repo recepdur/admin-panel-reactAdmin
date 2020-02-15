2a5838f53fb5f38b784e6f89c200ee3134202cb8
Recep Dur <recepdur@outlook.com>
2020-02-15 21:40:57 +0300

    theme added

--- src/App.js
+++ src/App.js
@@ -8,10 +8,32 @@ import PostIcon from "@material-ui/icons/Book"
 import UserIcon from "@material-ui/icons/Group"
 import HomeIcon from "@material-ui/icons/Home"
 import Dashboard from "./Dashboard"
+import { createMuiTheme } from '@material-ui/core/styles';
 
-const dataProvider = jsonServerProvider("http://jsonplaceholder.typicode.com")
+//const dataProvider = jsonServerProvider("http://jsonplaceholder.typicode.com")
+const dataProvider = jsonServerProvider("http://localhost:3001")
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
+
 const App = () => (
   <Admin
+    title="My Custom Admin"
+    theme={theme}
     dashboard={Dashboard}
     authProvider={authProvider}
     icon={HomeIcon}
